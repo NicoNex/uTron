9a4e2c81071ad4b4e15d3feb3e4a6282367749d3
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2019-01-26 12:48:05 +0100

    code cleanup and add makefile

--- network.c
+++ network.c
@@ -16,7 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 #include "network.h"
 
